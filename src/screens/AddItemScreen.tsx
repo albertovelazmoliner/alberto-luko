6ceccefd29e13799ab9467366d1a3b2776ed2b0c
Alberto Vélaz <albertovelaz.dev@gmail.com>
2022-02-21 11:33:20 +0100

    Update AddItemScreen.tsx

--- src/screens/AddItemScreen.tsx
+++ src/screens/AddItemScreen.tsx
@@ -1,18 +1,70 @@
-import { StyleSheet, View } from "react-native";
+import { Alert, StyleSheet, View } from "react-native";
+import { ValuableForm } from "../components/valuableForm/ValuableForm";
 
 import Button from "../components/Button";
 import { RootTabScreenProps } from "../navigation/types";
 import { colors } from "../theme/colors";
+import { useValuables } from "../hooks";
+import { useEffect, useState } from "react";
 
 export default function AddItemScreen({
   navigation,
 }: RootTabScreenProps<"AddItemScreen">) {
+  const { addValuable } = useValuables();
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState("");
+  const [price, setPrice] = useState("");
+
+  const [addButtonDisabled, setAddButtonDisabled] = useState(true);
+
+  useEffect(() => {
+    if (name.length > 0 && image.length > 0 && price !== null) {
+      setAddButtonDisabled(false);
+    } else {
+      setAddButtonDisabled(true);
+    }
+  }, [name, image, price]);
+
+  const showAlert = (message: string) =>
+    Alert.alert(
+      "Action failed",
+      message,
+      [
+        {
+          text: "Ok",
+          style: "default",
+        },
+      ]
+    );
+
+  const addValuableHandler = () => {
+    try {
+      addValuable(name, price, description, image);
+      navigation.goBack();
+    } catch (err: any) {
+      showAlert(err.message);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.buttonsContainer}>
         <Button title="Cancel" onPress={() => navigation.goBack()} />
-        <Button title="Add" disabled onPress={() => undefined} />
+        <Button
+          title="Add"
+          disabled={addButtonDisabled}
+          onPress={addValuableHandler}
+        />
       </View>
+      <ValuableForm
+        onChangeName={(name: string) => setName(name)}
+        onChangeDescription={(description: string) =>
+          setDescription(description)
+        }
+        onChangePhoto={(image: string) => setImage(image)}
+        onChangePrice={(price: string) => setPrice(price)}
+      />
     </View>
   );
 }
