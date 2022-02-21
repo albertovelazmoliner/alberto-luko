7cbe884364f0ea7036ad41ac8dcaab90db574a1f
Alberto Vélaz <albertovelaz.dev@gmail.com>
2022-02-21 11:33:27 +0100

    Update InventoryScreen.tsx

--- src/screens/InventoryScreen.tsx
+++ src/screens/InventoryScreen.tsx
@@ -1,17 +1,28 @@
-import { StyleSheet, View } from "react-native";
+import { useContext } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
 import { Title } from "../components/Title";
+import { ValuableCard } from "../components/valuableCard";
+import ValuableContext from "../context/ValuablesContext";
 import { RootTabScreenProps } from "../navigation/types";
 import { colors } from "../theme/colors";
 
 export default function InventoryScreen({
   navigation,
-  route
+  route,
 }: RootTabScreenProps<"Inventory">) {
   const handleAddButtonPress = () => navigation.navigate("AddItem");
 
+  const { valuables } = useContext(ValuableContext);
+
   return (
     <View style={styles.container}>
-        <Title onButtonPress={handleAddButtonPress}>{route.name}</Title>
+      <Title onButtonPress={handleAddButtonPress}>{route.name}</Title>
+      <FlatList
+        data={valuables}
+        renderItem={({ item }) => <ValuableCard item={item} />}
+        numColumns={2}
+        keyExtractor={(item) => `${item.id}`}
+      />
     </View>
   );
 }
@@ -21,5 +32,5 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 20,
     backgroundColor: colors.background,
-  }
+  },
 });
